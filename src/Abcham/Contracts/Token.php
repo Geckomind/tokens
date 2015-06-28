8e14633ee34439aabfaf2985f52630e752ec59ba
Abraham Chavez Jimenez <mindacj@gmail.com>
2015-06-28 10:00:09 -0500

    Versión Inicial

--- src/Abcham/Contracts/Token.php
+++ src/Abcham/Contracts/Token.php
@@ -1,13 +1,28 @@
 <?php
 
 namespace Abcham\Contracts;
+use DateTime;
 
 /**
 * Interfaz de Tokens
 */
 interface Token
 {
+    /**
+     * Regrsa la cadena criptoraficamente segura
+     * @return string
+     */
     public function token();
+
+    /**
+     * Regresa el onjeto DateTime con la fecha de creación
+     * @return DateTime
+     */
     public function createdAt();
+
+    /**
+     * Regresa el onjeto DateTime con la fecha de expiración
+     * @return DateTime
+     */
     public function validUntil();
 }
